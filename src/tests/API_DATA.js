4e1ef441a9e4f9d87f3f62eede9cdc247ccc62b4
Lukasz Ciesluk <lukaszciesluk@gmail.com>
2024-05-06 17:50:16 +0100

    More tests

---
+++ src/tests/API_DATA.js
@@ -0,0 +1,27 @@
+const INVESTORS_DATA_API = [
+  {
+    firm_id: 2670,
+    firm_name: 'Mjd Jedi fund',
+    AUM: 426920827,
+    date_added: '2010-06-08T00:00:00Z',
+    last_updated: '2024-02-21T00:00:00Z',
+    established_at: '2010-06-08T00:00:00Z',
+    firm_type: 'bank',
+    city: 'Hong Kong',
+    country: 'China',
+    address: '29 Nathan Road, Hong Kong',
+    postal_code: '37E',
+  },
+]
+
+const COMMITMENT_DATA_API = [
+  {
+    id: 46808,
+    firm_id: 2670,
+    asset_class: 'hf',
+    amount: '80M',
+    currency: 'USD',
+  },
+]
+
+export { INVESTORS_DATA_API, COMMITMENT_DATA_API }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import React from 'react'
 import '@testing-library/jest-dom'
 import App from '../App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 
 jest.mock('axios')
 
@@ -10,4 +10,16 @@ test('full app rendering/navigating default route', async () => {
   render(<App />, { wrapper: BrowserRouter })
 
   expect(screen.getByText(/Investor app/i)).toBeInTheDocument()
+})
+
+test('renders not existing page', () => {
+  const badRoute = '/not/existing/page'
+
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <App />
+    </MemoryRouter>
+  )
+
+  expect(screen.getByText(/Page not found/i)).toBeInTheDocument()
 })
