4446d13dbd3b4ace8a044878805a31b9743b594e
Lukasz Ciesluk <lukaszciesluk@gmail.com>
2024-05-06 16:58:27 +0100

    Adding commitment details component

--- src/investors/commitments/Commitments.js
+++ src/investors/commitments/Commitments.js
@@ -1,4 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import axios from 'axios'
+
+import CommitmentDetails from './CommitmentDetails'
+
 function Commitments() {
+    const { id } = useParams()
+
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [commitments, setCommitments] = useState([])
+
+    useEffect(() => {
+
+    }, []);
+
+    if (error) {
+        return <Error error={error} />
+    }
+
     return (
         <div>
             Commitments
