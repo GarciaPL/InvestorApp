9ef335641400f38a55743f1ae852e560014f98db
Lukasz Ciesluk <lukaszciesluk@gmail.com>
2024-05-06 17:46:12 +0100

    Adding first App test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,13 @@
 import { render, screen } from '@testing-library/react'
+import React from 'react'
+import '@testing-library/jest-dom'
 import App from '../App'
+import { BrowserRouter } from 'react-router-dom'
+
+jest.mock('axios')
+
+test('full app rendering/navigating default route', async () => {
+  render(<App />, { wrapper: BrowserRouter })
+
+  expect(screen.getByText(/Investor app/i)).toBeInTheDocument()
+})
